8b48147b1b7ccc061e08c52586655f3faad0f80a
clementD99 <clement.desmur@gmail.com>
2024-12-26 12:39:43 +0100

    Corrections et ajouts

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Composants/Header/Header";
+import Accueil from "./Composants/Accueil/Accueil";
 
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
