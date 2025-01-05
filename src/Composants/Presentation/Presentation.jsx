6980da7baee8c2b64487fd4ed2302cc31a5f0f92
clementD99 <clement.desmur@gmail.com>
2025-01-05 18:42:28 +0100

    Création de présentation pour le site

---
+++ src/Composants/Presentation/Presentation.jsx
@@ -0,0 +1,11 @@
+function Presentation() {
+    return (
+      <section>
+        <h1>Présentation</h1>
+        <p>Voici la section Présentation.</p>
+      </section>
+    );
+  }
+  
+  export default Presentation;
+  
