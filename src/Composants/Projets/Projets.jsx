d513d98e09eba2f5303c6bf8b451b54239a14071
clementD99 <clement.desmur@gmail.com>
2025-01-14 18:27:53 +0100

    Modifications pour incorporer les éléments

--- src/Composants/Projets/Projets.jsx
+++ src/Composants/Projets/Projets.jsx
@@ -1,7 +1,20 @@
-function Projets () {
-    return (
-        <div>Projets</div>
-    )
+import "./projets.scss";
+import projets from "../../donnees/projets.json";
+
+function Projets() {
+  return (
+    <div className="titre">
+      <h1>Projets</h1>
+      <section className="projets">
+        {projets.map((projet) => (
+          <div className="projets-details" key={projet.id}>
+            <h2 className="projets-titre">{projet.title}</h2>
+            {/* <p className="projets-description">{projet.description}</p> */}
+          </div>
+        ))}
+      </section>
+    </div>
+  );
 }
 
 export default Projets;
