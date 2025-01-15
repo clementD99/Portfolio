2896f028b604a213b3d79ff51dea6c5528739bf1
clementD99 <clement.desmur@gmail.com>
2025-01-15 18:58:03 +0100

    Modal + style pour la modal

--- src/Composants/Projets/Projets.jsx
+++ src/Composants/Projets/Projets.jsx
@@ -1,18 +1,72 @@
+import { useState } from "react";
 import "./projets.scss";
 import projets from "../../donnees/projets.json";
 
 function Projets() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+
+  // Pour ouvrir la modal
+  const openModal = (projet) => {
+    setModalContent(projet);
+    setIsModalOpen(true);
+  };
+
+  // Pour fermer la modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setModalContent(null);
+  };
+
   return (
     <div className="titre">
-      <h1>Projets</h1>
+      <h2>Mes projets</h2>
       <section className="projets">
         {projets.map((projet) => (
-          <div className="projets-details" key={projet.id}>
-            <h2 className="projets-titre">{projet.title}</h2>
-            {/* <p className="projets-description">{projet.description}</p> */}
+          <div
+            className="projets-details"
+            key={projet.id}
+            onClick={() => openModal(projet)}
+          >
+            <img
+              src={require(`../../assets/images/${projet.image}`)}
+              alt={`Illustration de ${projet.title}`}
+            />
           </div>
         ))}
       </section>
+
+      {/* Modal */}
+      {isModalOpen && modalContent && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <h3>{modalContent.title}</h3>
+            <p>{modalContent.description}</p>
+            <ul>
+              {modalContent.competences.map((competence, index) => (
+                <li key={index}>{competence}</li>
+              ))}
+            </ul>
+            <a
+              href={modalContent.lien}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Voir le projet sur GitHub
+            </a>
+            {modalContent["github-pages"] && (
+              <a
+                href={modalContent["github-pages"]}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Voir le projet en ligne
+              </a>
+            )}
+            <button onClick={closeModal}>Fermer</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
