0bd337739229bb5e5a3e6978249625d5d7507921
clementD99 <clement.desmur@gmail.com>
2025-01-21 11:07:39 +0100

    Ajout du fichier Acquis et style pour le fichier

--- src/Composants/Presentation/Presentation.jsx
+++ src/Composants/Presentation/Presentation.jsx
@@ -1,12 +1,5 @@
 import "./presentation.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHtml5,
-  faCss3Alt,
-  faJs,
-  faSass,
-  faReact,
-} from "@fortawesome/free-brands-svg-icons";
+import Acquis from "../Acquis/Acquis";
 
 function Presentation() {
   return (
@@ -34,48 +27,7 @@ function Presentation() {
         Au fil de mon parcours, j'ai appris les divers langages et logiciels
         suivants :
       </span>
-      <br />
-      <div className="acquis">
-        <FontAwesomeIcon
-          className="html"
-          title="logo HTML"
-          icon={faHtml5}
-        ></FontAwesomeIcon>
-        <span>
-          Le HTML pour écrire les textes, insérer des images. C'est la base de
-          toutes pages internet.
-        </span>
-        <FontAwesomeIcon
-          className="css"
-          title="logo CSS"
-          icon={faCss3Alt}
-        ></FontAwesomeIcon>
-        <span>
-          Le CSS pour styliser le HTML (pour les couleurs, espacer les lignes,
-          aligner des textes,...)
-        </span>
-        <FontAwesomeIcon
-          className="js"
-          title="logo JavaScript"
-          icon={faJs}
-        ></FontAwesomeIcon>
-        <span>JavaScript pour une interactivité sur la page.</span>
-        <FontAwesomeIcon
-          className="sass"
-          title="logo Sass"
-          icon={faSass}
-        ></FontAwesomeIcon>
-        <span>
-          Sass pour créer chaque feuille de style pour un fichier sans devoir
-          créer un fichier css de plusieurs centaines de lignes.
-        </span>
-        <FontAwesomeIcon
-          className="react"
-          title="logo React"
-          icon={faReact}
-        ></FontAwesomeIcon>
-        <span>React pour les interfaces utilisateurs web.</span>
-      </div>
+      <Acquis />
     </div>
   );
 }
